d5516bdec958b626b2d71c83f38aefe50b33dae5
Sebastián Čambál <83927605+sebocam@users.noreply.github.com>
2022-02-27 14:42:45 +0100

    Bugfixes, runnable

--- MTAA_zadanie1_CambalS.py
+++ MTAA_zadanie1_CambalS.py
@@ -1,25 +1,28 @@
 import logging
+import re
 import socket
 import socketserver
 import sys
 import time
-from sipfullproxy import *
+import sipfullproxy as sip
+
+
+HOST, PORT = '0.0.0.0', 5060
 
 
 def main():
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='proxy.log',level=logging.INFO,datefmt='%H:%M:%S')
-    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ", time.localtime()))
     hostname = socket.gethostname()
-    logging.info(hostname)
     ipaddress = socket.gethostbyname(hostname)
     if ipaddress == "127.0.0.1":
         ipaddress = sys.argv[1]
-    logging.info(ipaddress)
-    recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress,PORT)
-    topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress,PORT)
-    server = socketserver.UDPServer((HOST, PORT), UDPHandler)
+    logging.info('Hosting IP: %s' % ipaddress)
+    sip.recordroute = "Record-Route: <sip:%s:%d;lr>" % (ipaddress, PORT)
+    sip.topvia = "Via: SIP/2.0/UDP %s:%d" % (ipaddress, PORT)
+    server = socketserver.UDPServer((HOST, PORT), sip.UDPHandler)
     server.serve_forever()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', filename='MTAA_proxy.log', level=logging.INFO,datefmt='%H:%M:%S')
+    logging.info(time.strftime("%a, %d %b %Y %H:%M:%S ---> PROXY START", time.localtime()))
     main()
